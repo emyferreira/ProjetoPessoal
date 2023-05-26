2d65ed4aa9311f69835ff0677b6f002d0b094570
emyferreira <emily.ferreira@sptech.school>
2023-05-26 14:31:52 -0300

    Script 1.0 na pasta script-tabelas.sql

--- web-data-viz-main/site/src/database/config.js
+++ web-data-viz-main/site/src/database/config.js
@@ -2,25 +2,25 @@ var mysql = require("mysql2");
 var sql = require('mssql');
 
 // CONEXÃO DO SQL SERVER - AZURE (NUVEM)
-var sqlServerConfig = {
-    server: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
-    pool: {
-        max: 10,
-        min: 0,
-        idleTimeoutMillis: 30000
-    },
-    options: {
-        encrypt: true, // for azure
-    }
-}
+// var sqlServerConfig = {
+//     server: "SEU_SERVIDOR",
+//     database: "RainbowEye",
+//     user: "SEU_USUARIO",
+//     password: "SUA_SENHA",
+//     pool: {
+//         max: 10,
+//         min: 0,
+//         idleTimeoutMillis: 30000
+//     },
+//     options: {
+//         encrypt: true, // for azure
+//     }
+// }
 
 // CONEXÃO DO MYSQL WORKBENCH
 var mySqlConfig = {
     host: "SEU_SERVIDOR",
-    database: "SEU_BANCO_DE_DADOS",
+    database: "RainbowEye",
     user: "SEU_USUARIO",
     password: "SUA_SENHA",
 };
--- web-data-viz-main/site/src/database/script-tabelas.sql
+++ web-data-viz-main/site/src/database/script-tabelas.sql
@@ -6,82 +6,33 @@
 comandos para mysql - banco local - ambiente de desenvolvimento
 */
 
-CREATE DATABASE aquatech;
+CREATE DATABASE RainbowEye;
 
-USE aquatech;
+USE RainbowEye;
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+CREATE TABLE Usuário (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nomeCompleto VARCHAR(45),
+    nomeUser VARCHAR(15),
+    senha VARCHAR(30),
+    biografia VARCHAR(50)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
+CREATE TABLE Perfil (
+	idPerfil INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(40),
+    telCelular CHAR(11),
+    fkUsuario INT,
+    FOREIGN KEY (fkUsuario) REFERENCES Usuario(idUsuario)
+) AUTO_INCREMENT = 100;
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-
-/*
-comando para sql server - banco remoto - ambiente de produção
-*/
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-);
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY IDENTITY(1,1),
-	descricao VARCHAR(300)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-CREATE TABLE medida (
-	id INT PRIMARY KEY IDENTITY(1,1),
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
-);
+CREATE TABLE Registro (
+	idRegistro INT PRIMARY KEY AUTO_INCREMENT,
+    imgRegistro VARCHAR(50),
+    legenda VARCHAR(35),
+    fkPerfil INT,
+    FOREIGN KEY (fkPerfil) REFERENCES Perfil(idPerfil)
+) AUTO_INCREMENT = 1000;
 
 /*
 comandos para criar usuário em banco de dados azure, sqlserver,
