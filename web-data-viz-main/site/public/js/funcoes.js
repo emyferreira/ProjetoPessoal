78c9494ce727b05fed5af49a2c6144ce036c58df
emyferreira <emily.ferreira@sptech.school>
2023-05-29 14:10:49 -0300

    API começo da implementação

--- web-data-viz-main/site/public/js/funcoes.js
+++ web-data-viz-main/site/public/js/funcoes.js
@@ -13,7 +13,7 @@ function validarSessao() {
 
         // finalizarAguardar();
     } else {
-        window.location = "../login.html";
+        window.location = "assets/login.html";
     }
 }
 
@@ -21,7 +21,7 @@ function limparSessao() {
     // aguardar();
     sessionStorage.clear();
     // finalizarAguardar();
-    window.location = "../login.html";
+    window.location = "assets/login.html";
 }
 
 // carregamento (loading)
